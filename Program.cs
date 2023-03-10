84a254dcea7f793b8ec0278f9fe4067e0f20575b
Sergei Schneidmiller <5797221@gmail.com>
2023-03-10 16:41:42 +0300

    dz3 ready

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+double i = num % 2;
+if (i == 0)
+{
+    Console.WriteLine ("Это число - четное");
+}
+else
+{
+    Console.WriteLine ("Это число - нечетное");
+}
